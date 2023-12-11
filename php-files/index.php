367183e816069aeeb00876441c4dc174c7774bb3
FadjriBoyang <117616974+FadjriBoyang@users.noreply.github.com>
2023-12-11 21:28:59 +0700

    First

---
+++ php-files/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php echo '<p>FADJRI</p>'; ?>
+    <img src="https://s5.gifyu.com/images/SRLLN.gif" alt="Coding Cat">
+</body>
+</html>
